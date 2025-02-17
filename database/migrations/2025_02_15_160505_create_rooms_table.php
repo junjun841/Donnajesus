c65362315308aeee3a79bc613724abd1a5c0ddaa
GeildanLozada <occ.lozada.geildan@gmail.com>
2025-02-17 08:33:51 +0800

    first commit

--- database/migrations/2025_02_15_160505_create_rooms_table.php
+++ database/migrations/2025_02_15_160505_create_rooms_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id('room_id');
+            $table->string('room_number');
+            $table->string('room_type');
+            $table->string('price');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('rooms');
     }
 };
